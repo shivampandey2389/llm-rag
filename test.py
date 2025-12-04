a300dc4c2cbc636a2a91d8c37952c18dc9dd3846
Shivam Pandey <shivampandey2389@gmail.com>
2025-12-04 12:09:32 +0530

    retreival successfull

--- test.py
+++
@@ -1,7 +0,0 @@
-from groq import Groq
-
-client = Groq(api_key="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")
-
-resp = client.models.list()
-print(resp)
-print("Successfully listed Groq models.")
